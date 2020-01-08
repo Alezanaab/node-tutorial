e29262805dbad771da567c2c51e807f918b3f7f1
AlezanaAbrego <aabrego@unitedvirtualities.com>
2020-01-08 14:56:20 -0700

    Using NPM

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-require('./app/index');
+/*GETTING STARTED WITH NODE*/
+// require('./app/index');
+
+
+/*USING NPM*/
+const _ = require('lodash')
+
+_.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 });
+// → { 'a': 1, 'b': 2, 'c': 3 }
